f2a6075d0911940e91dbc2adc5ec69fa4a0a1e75
Matt Raible <matt.raible@okta.com>
2019-09-19 09:05:58 -0600

    Re-add client

---
+++ client/src/app.config.js
@@ -0,0 +1,6 @@
+export default {
+  url: 'https://dev-133320.okta.com',
+  issuer: 'https://dev-133320.okta.com/oauth2/default',
+  redirect_uri: window.location.origin + '/implicit/callback',
+  client_id: '0oa1cuzmwddDSQUub357'
+};
